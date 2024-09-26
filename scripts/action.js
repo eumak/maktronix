106b40f75457cb610aec7417c0b64c2450546d81
eumak <38827031+eumak@users.noreply.github.com>
2024-09-26 02:21:34 -0400

    Responsive setup
    Added responsive views

--- scripts/action.js
+++ scripts/action.js
@@ -12,11 +12,21 @@ function loadPage(page) {
     services.style.display = page !== 'services' ? 'none' : 'block';
     industries.style.display = page !== 'industry' ? 'none' : 'block';
     contact.style.display = page !== 'contact' ? 'none' : 'block';
+    
+    // Collapse the menu after a link is clicked
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.remove('active');
 }
 
 function setCurrentYear() {
     const currentYear = new Date().getFullYear();
     document.getElementById("current-year").textContent = currentYear;
+}
+
+// Function to toggle the navigation menu
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
 }
 
 // Load the home page by default when the page loads
